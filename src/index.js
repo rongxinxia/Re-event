14cca057c82b49e3e2a2add93d5ee3769f37ffa1
rongxinxia <rongxinxia01@gmail.com>
2018-07-18 20:09:47 -0400

    initial

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const root = document.getElementById('root');
+
+let render = () =>{
+    ReactDOM.render(<App />, root);
+};
+
+if(module.hot){
+    module.hot.accept('./App',()=>{
+        setTimeout(render);
+    })
+}
+
+render();
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
